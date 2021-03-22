b373109c4c784459b8c25402c2efb55aa9adc01d
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-03-22 18:30:26 +0100

    String Interpolation

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,2 +1,3 @@
-let favoriteAnimal = "bear";
-console.log("My favorite animal: "+ favoriteAnimal);
+let myName = 'Marie';
+let myCity = 'Leipzig';
+console.log(`My name is ${myName}.My favorite city is ${myCity}.`)
