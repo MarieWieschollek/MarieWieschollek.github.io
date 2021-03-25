62bb5ad5a6616e1811a1f9e96c864adcf5676f2a
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-03-25 16:13:27 +0100

    tileLayer Streetmap und Zoom

--- nz/main.js
+++ nz/main.js
@@ -1,3 +1,10 @@
 
-const map = L.map("map");
+const map = L.map("map", { 
+    center: [ -45.031111, 168.6625 ],
+    zoom: 13,
+    layers: [ 
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+     ]
+} );
+
 console.log(document.querySelector("#map"));
