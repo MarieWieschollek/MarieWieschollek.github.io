a227749607c5fd92b6e50a01f14d480e1f5cef1d
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-03-25 17:12:04 +0100

    Route

--- nz/main.js
+++ nz/main.js
@@ -23,7 +23,6 @@ mrk.bindPopup(`
 <h4>Stop ${stop.nr}: ${stop.name} </h4>
 <p> <i class="fas fa-external-link-alt mr-3"></i> <a href="${stop.wikipedia}"> Read about stop in Wikipedia </a> </p>
     
-`.openPopup();
-
+`).openPopup();
 
 console.log(document.querySelector("#map"));
