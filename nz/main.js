407c9f71de668bef70cd698b99dc78bb889bcf43
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-03-25 16:34:43 +0100

    Stop Objekt erzeugt und verwendet

--- nz/main.js
+++ nz/main.js
@@ -1,13 +1,24 @@
 
+let stop = {
+nr: 12,
+name: "Queenstown",
+lat: -45.031111,
+lng: 168.6625,
+user: "MarieWieschollek",
+wikipedia: "https://en.wikipedia.org/wiki/Queenstown,_New_Zealand",
+
+};
+
+
 const map = L.map("map", { 
-    center: [ -45.031111, 168.6625 ],
+    center: [ stop.lat, stop.lng ],
     zoom: 13,
     layers: [ 
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
      ]
 } );
 
-let mrk = L.marker([ -45.031111, 168.6625 ]).addTo(map);
+let mrk = L.marker([ stop.lat, stop.lng ]).addTo(map);
 mrk.bindPopup("Queenstown").openPopup();
 
 
