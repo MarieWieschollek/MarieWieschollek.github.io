2a55b4798d9ec87b8a7c98847cf378371c133f67
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-03-25 17:31:08 +0100

    Eigener Marker

--- nz/main.js
+++ nz/main.js
@@ -27,8 +27,12 @@ mrk.bindPopup(`
 <h4>Stop ${entry.nr}: ${entry.name} </h4>
 <p> <i class="fas fa-external-link-alt mr-3"></i> <a href="${entry.wikipedia}"> Read about stop in Wikipedia </a> </p>
     
-`).openPopup();
-}
+`);
+
+if(entry.nr == 12) {
+    mrk.openPopup();
+}}
+
 
 
 console.log(document.querySelector("#map"));
