a5c02fe072b7fb85e6a0ec58ad1b43261dd4e3dc
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-04-15 17:37:13 +0200

    plot

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -46,6 +46,7 @@ fetch(awsUrl)
             <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
             <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
             </ul>
+            <a href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
             marker.addTo(awsLayer);
         }
