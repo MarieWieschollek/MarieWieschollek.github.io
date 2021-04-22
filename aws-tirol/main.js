4f3187ee8d8e79c53ac4a9fb8906c0629c3d141b
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-04-22 17:36:48 +0200

    und nochmal

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,7 +1,7 @@
-// https://leafletjs.com/reference-1.7.1.html#tilelayer
+//https://leafletjs.com/reference-1.7.1.html#tilelayer
 let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
-// https://leafletjs.com/reference-1.7.1.html#map-example
+//https://leafletjs.com/reference-1.7.1.html#map-example
 let map = L.map("map", {
     center: [47, 11],
     zoom: 9,
@@ -10,113 +10,111 @@ let map = L.map("map", {
     ]
 });
 
+//https://leafletjs.com/reference-1.7.1.html#layer
+
 let overlays = {
-    stations: L.featureGroup(),
-    temperature: L.featureGroup(),
-    snowheight: L.featureGroup(),
-    windspeed: L.featureGroup(),
-    winddirection: L.featureGroup()
+stations: L.featureGroup(),
+temperature: L.featureGroup(),
+snowheight: L.featureGroup(),
+windspeed: L.featureGroup(),
+winddirection: L.featureGroup(),
 };
 
-
-// https://leafletjs.com/reference-1.7.1.html#control
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    // https://leafletjs.com/reference-1.7.1.html#tilelayer
     "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
-    // https://leafletjs.com/reference-1.7.1.html#tilelayer
     "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
-    //https://leafletjs.com/reference-1.7.1.html#layergroup
+    "BasemapAT.overlay": L.tileLayer.provider('BasemapAT.overlay'),
     "BasemapAT.overlay+ortho": L.layerGroup([
-        // https://leafletjs.com/reference-1.7.1.html#tilelayer
         L.tileLayer.provider('BasemapAT.orthofoto'),
-        // https://leafletjs.com/reference-1.7.1.html#tilelayer
         L.tileLayer.provider('BasemapAT.overlay')
     ])
-},{
-    "Wetterstationen Tirol": overlays.stations,
-    "Lufttemperatur (°C)": overlays.temperature,
-    "Schneehöhen (cm)": overlays.snowheight,
-    "Windgeschwindigkeit (km/h)": overlays.windspeed,
-    "Windrichtung": overlays.winddirection
-
-} , {
+}, {
+    'Wetterstationen Tirol': overlays.stations,
+    'Lufttemperatur (°C)': overlays.temperature,
+    'Schneehöhe (cm)': overlays.snowheight,
+    'Windgeschwindigkeit (km/h)': overlays.windspeed,
+    'Windrichtung': overlays.winddirection
+}, {
     collapsed: false
+}).addTo(map);
+overlays.temperature.addTo(map);
 
-}  ).addTo(map);
-
-    overlays.temperature.addTo(map);
-
-    L.control.scale({
+L.control.scale(
+    {
+        maxWidth: 200,
+        metric: true,
         imperial: false
-    }).addTo(map);
+    }
+).addTo(map);
 
-    let newLabel = (coords, options) => {
-        console.log("Koordinaten coords: ", coords);
-        console.log("Optionsobjekt:", options);
-        let label = L.divIcon( {
+let newLabel = (coords, options) => {
+    let label = L.divIcon({
         html: `<div>${options.value}</div>`,
         className: "text-label"
-        })
-        let marker = L.marker([coords[1], coords[0]]);
-        console.log("Marker:", marker);
-        return marker;
-    };
+    })
+    let marker = L.marker([coords[1],coords[0]], {
+        icon: label
+    });
+    return marker;
+    //Label zurückgeben
+};
+// .text-label div
+let awsUrl = "https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson";
 
+//https://leafletjs.com/reference-1.7.1.html#featuregroup
 
-let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
-
-fetch(awsUrl)
-    .then(response => response.json())
+fetch(awsUrl).then(response => response.json())
     .then(json => {
-        console.log('Daten konvertiert: ', json);
+        console.log("Daten konvertiert: ", json);
         for (station of json.features) {
-            // console.log('Station: ', station);
-            //https://leafletjs.com/reference-1.7.1.html#marker
+            console.log("Station: ", station);
             let marker = L.marker([
                 station.geometry.coordinates[1],
                 station.geometry.coordinates[0]
             ]);
             let formattedDate = new Date(station.properties.date);
             marker.bindPopup(`
-            <h3>${station.properties.name}</h3>
-            <ul>
-              <li>Datum: ${formattedDate.toLocaleString("de")}</li>
-              <li>Seehöhe: ${station.geometry.coordinates[2]} m</li>
-              <li>Temperatur: ${station.properties.LT} C</li>
-              <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>
-              <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
-              <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
-            </ul>
-            <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
-            `);
+    <h3>${station.properties.name}</h3>
+    <ul>
+        <li>Datum: ${formattedDate.toLocaleString("de")} Uhr</li>
+        <li>Seehöhe: ${station.geometry.coordinates[2] ||"?"} m.ü.A.</li>
+        <li>Temperatur: ${station.properties.LT||"?"} °C</li>
+        <li>Relative Luftfeuchtigkeit: ${station.properties.RH||"?"} %</li>
+        <li>Schneehöhe: ${station.properties.HS||"?"} cm</li>
+        <li>Windgeschwindigkeit: ${station.properties.WG||"?"} km/h</li>
+        <li>Windrichtung: ${station.properties.WR||"?"} °</li>
+    </ul>
+    <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
+    `);
+            //marker.addTo(awsLayer);
             marker.addTo(overlays.stations);
             //Schneehöhe
-             if (typeof station.properties.HS == 'number'){
+            if (typeof station.properties.HS == 'number'){
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.HS
                 });
                 marker.addTo(overlays.snowheight);
-            } ;
-            
-          //Windgeschwindigkeit
+            } 
+            //Windgeschwindigkeit
             if(typeof station.properties.WG == 'number'){
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.WG
                 });
                 marker.addTo(overlays.windspeed);
-            } ;
-            
-         //Lufttemperatur 
+            }
+            //Lufttemperatur 
             if(typeof station.properties.LT == 'number'){
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.LT
                 });
                 marker.addTo(overlays.temperature);
-            } ;
+            }
+        }
         // set map view to all stations
         map.fitBounds(overlays.stations.getBounds());
     });
 
-   
+    
+            
    
