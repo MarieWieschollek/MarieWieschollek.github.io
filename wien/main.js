2cad547775e0d721e3b2087e8b7adcfa7d78d846
MarieWieschollek <Marie.Wieschollek@student.uibk.ac.at>
2021-05-06 18:14:32 +0200

    farbige Intervallpoligone

--- wien/main.js
+++ wien/main.js
@@ -168,6 +168,19 @@ var miniMap = new L.Control.MiniMap(
 
 let styleIntervals = (feature) => { 
 console.log(feature.properties);
+console.log(feature.properties.Measure)
+let color = "";
+let range = feature.properties.Range;
+if (feature.properties.Measure === "time") { 
+    color = COLORS.minutes[range];
+} else if (feature.properties.Measure === "distance") { 
+    color = COLORS.kilometers[range]; }
+    else { color ="black" }
+    return {
+        color: color,
+        opacity: 0.5,
+        fillOpacity: 0.2
+    };
 };
 
 L.control.reachability({
